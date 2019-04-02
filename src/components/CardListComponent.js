393704388320c9bf6e163d0c67467d6d7f0263f7
gitoshh <karani.gitonga.godwin@gmail.com>
2019-04-02 14:27:02 +0300

    Add some styling

--- src/components/CardListComponent.js
+++ src/components/CardListComponent.js
@@ -5,6 +5,8 @@ export class CardListComponent extends Component {
     render() {
       return (
         <div>
+            <div className='header-list'>Github Users</div>
+        <hr className='line'/>
           {this.props.profiles.map(profile => <CardComponent key={profile.id} {...profile} />)}
         </div>
       )
